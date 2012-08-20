4f467ba47c903eda9efe4ea6e1b691becf4dd3cb
Mike Michaelevich <mmichael0413@gmail.com>
2012-08-20 17:59:52 -0400

    add it all baby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrunkenNemesis::Application.config.secret_token = '5d0a34dadd196c3b9ec27f552ff2b0b617eaf7f37f081cfbd202def25540e1da7b3fd7c809805eff6087edbc3ddde9cf321ec22a613a73fd6f827de279994752'
